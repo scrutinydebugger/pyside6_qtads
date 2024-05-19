57675224bce35cdd7e7875a6eaa1e5c4b7cbb2e5
Nicolas ELIE <nicolas.elie@cnrs.fr>
2024-05-19 07:48:15 +0200

    Sync with 4.3.0

--- setup.py
+++ setup.py
@@ -86,8 +86,9 @@ setuptools.setup(
             cmake_configure_options=[
                 "-DBUILD_EXAMPLES:BOOL=OFF",
                 "-DBUILD_STATIC:BOOL=ON",
-                "-DADS_VERSION=4.2.1",
-                f"-DPython3_ROOT_DIR={Path(sys.prefix)}"
+                "-DADS_VERSION=4.3.0",
+                f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
+                f"-DPython_EXECUTABLE={Path(sys.executable)}"
             ],
             py_limited_api=True
         ),
