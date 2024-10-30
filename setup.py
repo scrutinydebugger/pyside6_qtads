12592fca570b8826746ecfb84d09500728d7d4d3
Matt Borgerson <contact@mborgerson.com>
2024-10-29 21:06:06 -0700

    Update platform tag to cp39

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class bdist_wheel_abi3(bdist_wheel):
 
         if python.startswith("cp"):
             # on CPython, our wheels are abi3 and compatible back to 3.8
-            return "cp38", "abi3", plat
+            return "cp39", "abi3", plat
 
         return python, abi, plat
 
